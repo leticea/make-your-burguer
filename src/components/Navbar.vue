d1d1b2100f1265ed574362352b8b659d7672bd87
leticea <nascimento.leticeah@gmail.com>
2023-07-03 10:18:18 -0300

    feat(Navbar): styles

--- src/components/Navbar.vue
+++ src/components/Navbar.vue
@@ -1,6 +1,6 @@
 <template>
   <nav>
-    <router-link to="/">
+    <router-link to="/" id="logo_url">
       <img :src="logo" :alt="alt" id="logo" />
     </router-link>
     <router-link to="/">Home</router-link> |
@@ -13,4 +13,34 @@ export default {
   props: ["logo", "alt"],
 };
 </script>
-<style scoped></style>
+<style scoped>
+nav {
+  background-color: #222;
+  border-bottom: 4px solid #111;
+  padding: 15px 50px;
+  display: flex;
+  justify-content: flex-end;
+  align-items: center;
+}
+
+nav #logo_url {
+  margin: auto;
+  margin-left: 0;
+}
+
+#logo {
+  width: 40px;
+  height: 40px;
+}
+
+nav a {
+  color: #fcba03;
+  text-decoration: none;
+  margin: 12px;
+  transition: 0.5s;
+}
+
+nav a:hover {
+  color: #fff;
+}
+</style>

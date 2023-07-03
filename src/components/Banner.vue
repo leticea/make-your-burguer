c6f08aea0b207a92b2f05af5b8c4400333409998
leticea <nascimento.leticeah@gmail.com>
2023-07-03 11:35:39 -0300

    feat(Banner): creating banner component

---
+++ src/components/Banner.vue
@@ -0,0 +1,22 @@
+<template>
+  <div id="main-banner">
+    <h1>Make Your Burguer</h1>
+  </div>
+</template>
+<script>
+export default {
+  name: "Banner",
+};
+</script>
+
+<style scoped>
+#main-banner {
+  background-image: url("/public/img/burguer.jpg");
+  background-position: 0 -250px;
+  background-size: cover;
+  height: 500px;
+  display: flex;
+  align-items: center;
+  justify-content: flex-start;
+}
+</style>

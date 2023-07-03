b496763d1db34cea4433369f0004d24651b8d78e
leticea <nascimento.leticeah@gmail.com>
2023-07-03 12:44:03 -0300

    feat(Form): styles

--- src/components/Form.vue
+++ src/components/Form.vue
@@ -5,7 +5,13 @@
       <form id="burger-form">
         <div class="input-container">
           <label for="name">Nome do cliente</label>
-          <input type="text" id="name" name="name" v-model="name" placeholder="Digite o seu nome">
+          <input
+            type="text"
+            id="name"
+            name="name"
+            v-model="name"
+            placeholder="Digite o seu nome"
+          />
         </div>
         <div class="input-container">
           <label for="bread">Escolha o pão:</label>
@@ -21,15 +27,22 @@
             <option value="maminha">Maminha</option>
           </select>
         </div>
-        <div class="input-container">
-          <label for="bread">Selecione os opcionais:</label>
+        <div id="optional-container" class="input-container">
+          <label id="optional-title" for="optional"
+            >Selecione os opcionais:</label
+          >
           <div class="checkbox-container">
-            <input type="checkbox" name="optional" v-model="optional" value="salame">
+            <input
+              type="checkbox"
+              name="optional"
+              v-model="optional"
+              value="salame"
+            />
             <span>Salame</span>
           </div>
         </div>
         <div class="input-container">
-          <input type="submit" class="submit-btn" value="Criar meu Burguer!">
+          <input type="submit" class="submit-btn" value="Criar meu Burguer!" />
         </div>
       </form>
     </div>
@@ -41,3 +54,73 @@ export default {
   name: "Form",
 };
 </script>
+
+<style scoped>
+#burger-form {
+  max-width: 400px;
+  margin: 0 auto;
+}
+.input-container {
+  display: flex;
+  flex-direction: column;
+  margin-bottom: 20px;
+}
+
+label {
+  font-weight: bold;
+  margin-bottom: 15px;
+  color: #222;
+  padding: 5px 10px;
+  border-left: 4px solid #fcba03;
+}
+
+input,
+select {
+  padding: 5px 10px;
+  width: 300px;
+}
+
+#optional-container {
+  flex-direction: row;
+  flex-wrap: wrap;
+}
+
+#optional-title {
+  width: 100%;
+}
+
+.checkbox-container {
+  display: flex;
+  align-items: flex-start;
+  width: 50%;
+  margin-bottom: 20px;
+}
+
+.checkbox-container span,
+.checkbox-container input {
+  width: auto;
+}
+
+.checkbox-container span {
+  margin-left: 6px;
+  font-weight: bold;
+}
+
+.submit-btn {
+  background-color: #222;
+  color: #fcba03;
+  font-weight: bold;
+  border: 2px solid #222;
+  padding: 10px;
+  font-size: 16px;
+  margin: 0 auto;
+  cursor: pointer;
+  transition: 0.5s;
+  border-radius: 6px;
+}
+
+.submit-btn:hover {
+  background-color: transparent;
+  color: #222;
+}
+</style>

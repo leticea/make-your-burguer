67cf8a87ee23fa4547be0e2d3c612cfae482e3e7
leticea <nascimento.leticeah@gmail.com>
2023-07-03 09:56:16 -0300

    feat(components): creating Navbar component

--- src/App.vue
+++ src/App.vue
@@ -1,11 +1,16 @@
 <template>
-  <nav>
-    <router-link to="/">Home</router-link> |
-    <router-link to="/about">About</router-link>
-  </nav>
-  <router-view/>
+  <Navbar />
+  <router-view />
 </template>
 
-<style>
+<script>
+import Navbar from "./components/Navbar.vue";
 
-</style>
+export default {
+  components: {
+    Navbar
+  }
+}
+</script>
+
+<style></style>
---
+++ src/components/Navbar.vue
@@ -0,0 +1,15 @@
+<template>
+  <nav>
+    <router-link to="/">
+      <img src="" id="logo">
+    </router-link>
+    <router-link to="/">Home</router-link> |
+    <router-link to="/about">About</router-link>
+  </nav>
+</template>
+<script>
+export default {
+  name: "Navbar"
+};
+</script>
+<style scoped></style>
